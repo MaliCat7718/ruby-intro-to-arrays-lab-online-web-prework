0b011ad0e7e6807d2842289fe2f33bf888223de5
Malary Robinson <robinmal16@gmail.com>
2019-04-28 21:50:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,8 +9,4 @@ end
 
 def first_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-end
-
-def my_first_element
-  my_first_element = first_element(taylor_swift[0])
-end
+  taylor_swift.index["Welcome to New York"]
