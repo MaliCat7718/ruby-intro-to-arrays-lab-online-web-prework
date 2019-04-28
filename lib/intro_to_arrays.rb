9a95ff01d68fd84ec5539a818ebf2abcf3aad848
Malary Robinson <robinmal16@gmail.com>
2019-04-28 21:35:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,8 +7,10 @@ def array_with_two_elements
   array = ["Scamp", "Pepper"]
 end
 
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 def first_element
-  my_first_element = first_element(taylor_swift)
-  my_first_element[0]
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
+
+def my_first_element
+  my_first_element = first_element(taylor_swift[0])
 end
