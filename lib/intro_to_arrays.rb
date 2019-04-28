ef95c7f7a7485e844c2f2bc33381843efe3a7357
Malary Robinson <robinmal16@gmail.com>
2019-04-28 21:54:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,3 +14,7 @@ end
 def third_element(taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"])
   taylor_swift[2]
 end
+
+def last_element(taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"])
+  taylor_swift[3]
+end
