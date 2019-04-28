8160072e48c2d449861db67682b98c52a0fb1b35
Malary Robinson <robinmal16@gmail.com>
2019-04-28 21:51:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,6 @@ def array_with_two_elements
   array = ["Scamp", "Pepper"]
 end
 
-def first_element
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift.index["Welcome to New York"]
+def first_element(taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"])
+  taylor_swift[0]
 end
