0457486257da420be7876e6496fd64ae0fc5e29b
Malary Robinson <robinmal16@gmail.com>
2019-04-28 21:56:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,3 +18,8 @@ end
 def last_element(taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"])
   taylor_swift[3]
 end
+
+def first_element_with_array_methods(south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"])
+  south_east_asia.first
+end
+
